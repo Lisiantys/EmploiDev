4896ae4d7fc36ba5b9637d1dbe24be6c406c5e9f
Lisiantys <maxime.pouzargue7@gmail.com>
2024-10-13 18:45:02 +0200

    V3.2.4 - Style job offers modal + responsive

--- resources/js/components/JobOfferModal.vue
+++ resources/js/components/JobOfferModal.vue
@@ -1,125 +1,128 @@
-<!-- JobOfferModal.vue -->
 <template>
     <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50">
-        <div class="bg-white rounded-lg shadow-lg max-w-4xl w-full mx-4 md:mx-0">
+        <div
+            class="bg-white rounded-lg shadow-lg w-11/12 md:max-w-3xl lg:max-w-4xl xl:max-w-6xl md:w-full mx-4 md:mx-0">
             <!-- Vérifier si les données de l'offre d'emploi sont disponibles -->
             <div v-if="jobOffer && jobOffer.name">
-                <!-- Modal Header -->
-                <div class="flex justify-between items-center px-6 py-4 border-b">
-                    <h2 class="text-xl font-semibold">{{ jobOffer.name }}</h2>
-                    <button @click="closeModal" class="text-gray-700 hover:text-gray-900">
-                        <!-- Icône de fermeture -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
                 <!-- Modal Body -->
                 <div class="p-6">
-                    <!-- Job Offer Details -->
                     <div class="flex flex-col md:flex-row">
-                        <!-- Company Logo -->
-                        <div class="md:w-1/3">
-                            <img :src="companyLogoUrl" alt="Company Logo"
-                                class="w-full h-auto rounded-lg object-cover" />
+                        <!-- Logo de l'entreprise -->
+                        <div class="hidden md:block md:w-1/3">
+                            <img :src="companyLogoUrl" alt="Company Logo" class="w-full h-64 rounded-lg object-cover">
                         </div>
-                        <!-- Job Information -->
+                        <!-- Informations de l'offre -->
                         <div class="md:w-2/3 md:pl-6 mt-4 md:mt-0">
-                            <p v-if="jobOffer.company">
-                                <strong>Entreprise :</strong> {{ jobOffer.company.name }}
-                            </p>
-                            <p v-if="jobOffer.types_contract">
-                                <strong>Type de Contrat :</strong> {{ jobOffer.types_contract.name }}
-                            </p>
-                            <p v-if="jobOffer.types_developer">
-                                <strong>Type de Développeur Recherché :</strong> {{ jobOffer.types_developer.name }}
-                            </p>
-                            <p v-if="jobOffer.years_experience">
-                                <strong>Années d'Expérience Requises :</strong> {{ jobOffer.years_experience.name }}
-                            </p>
-                            <p v-if="jobOffer.location">
-                                <strong>Lieu :</strong> {{ jobOffer.location.city }}, {{ jobOffer.location.postal_code
-                                }}
-                            </p>
-                            <p>
-                                <strong>Description :</strong> {{ jobOffer.description }}
-                            </p>
-                            <!-- Programming Languages -->
-                            <div class="mt-4"
-                                v-if="jobOffer.programming_languages && jobOffer.programming_languages.length">
-                                <p><strong>Langages de Programmation Requis :</strong></p>
-                                <div class="flex flex-wrap mt-2">
-                                    <span v-for="language in jobOffer.programming_languages" :key="language.id"
-                                        class="bg-gray-200 text-gray-800 text-sm font-semibold mr-2 mb-2 px-2.5 py-0.5 rounded">
-                                        {{ language.name }}
-                                    </span>
+                            <h2 class="text-xl font-semibold mt-2">// {{ jobOffer.name }}</h2>
+                            <p class="text-wrap break-words text-xs md:text-sm lg:text-base font-light">{{
+                                jobOffer.description }}</p>
+                            <div class="flex flex-col mt-2 md:mt-0">
+                                <div>
+                                    <p v-if="jobOffer.company" class="text-xs md:text-sm mt-2">
+                                        <strong>Entreprise :</strong> {{ jobOffer.company.name }}
+                                    </p>
+                                    <p v-if="jobOffer.types_contract" class="text-xs md:text-sm mt-2">
+                                        <strong>Contrat :</strong> {{ jobOffer.types_contract.name }}
+                                    </p>
+                                </div>
+                                <div>
+                                    <p v-if="jobOffer.types_developer" class="text-xs md:text-sm mt-2">
+                                        <strong>Développeur Recherché :</strong> {{ jobOffer.types_developer.name }}
+                                    </p>
+                                    <p v-if="jobOffer.years_experience" class="text-xs md:text-sm mt-2">
+                                        <strong>Expérience Requise :</strong> {{ jobOffer.years_experience.name }}
+                                    </p>
+                                    <p v-if="jobOffer.location" class="text-xs md:text-sm mt-2">
+                                        <strong>Lieu :</strong> {{ jobOffer.location.city }}, {{
+                                        jobOffer.location.postal_code }}
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <!-- Langages de Programmation -->
+                    <div class="my-4" v-if="jobOffer.programming_languages && jobOffer.programming_languages.length">
+                        <div class="flex flex-wrap mt-2 md:max-h-none max-h-24 overflow-y-auto">
+                            <span v-for="language in jobOffer.programming_languages" :key="language.id"
+                                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs md:text-sm font-normal text-gray-700 mr-2 mb-2">
+                                #{{ language.name }}
+                            </span>
+                        </div>
+                    </div>
 
-                    <!-- Application Form visible only to authenticated developers -->
+                    <!-- Formulaire de candidature visible uniquement pour les développeurs authentifiés -->
                     <div v-if="authStore.user && authStore.user.role_id === 1">
-                        <!-- Show message if user has already applied -->
+                        <!-- Message si déjà postulé -->
                         <div v-if="hasAlreadyApplied">
-                            <p class="text-red-500 font-bold">
+                            <p class="text-blue-600 font-normal text-center text-xs md:text-base">
                                 Vous avez déjà postulé à cette offre d'emploi.
                             </p>
                         </div>
-                        <!-- Show application form if user hasn't applied yet -->
+                        <!-- Formulaire de candidature -->
                         <div v-else>
-                            <p class="text-gray-600 mb-4">
+                            <p class="text-blue-600 mb-4 font-normal text-center text-xs md:text-base">
                                 Si vous ne fournissez pas de CV ou de lettre de motivation, ceux de votre profil seront
                                 utilisés par défaut.
                             </p>
                             <form @submit.prevent="submitApplication">
                                 <!-- Description -->
                                 <div class="mb-4">
-                                    <label class="block text-gray-700">Description (obligatoire)</label>
+                                    <label class="block text-gray-700 text-xs md:text-sm">Description
+                                        (obligatoire)</label>
                                     <textarea v-model="applicationForm.description"
-                                        class="w-full border rounded px-3 py-2" rows="3" required></textarea>
-                                    <p v-if="errors.description" class="text-red-500 text-sm mt-1">
+                                        class="w-full border rounded px-3 py-2 text-xs md:text-sm" rows="3" required
+                                        minlength="20" maxlength="1024"></textarea>
+                                    <p v-if="errors.description" class="text-red-500 text-xs md:text-sm mt-1">
                                         {{ errors.description[0] }}
                                     </p>
                                 </div>
                                 <!-- CV -->
                                 <div class="mb-4">
-                                    <label class="block text-gray-700">CV (optionnel)</label>
-                                    <input type="file" @change="handleFileUpload($event, 'cv')"
-                                        class="w-full border rounded px-3 py-2" />
-                                    <p v-if="errors.cv" class="text-red-500 text-sm mt-1">
+                                    <label class="block text-gray-700 text-xs md:text-sm">CV (optionnel)</label>
+                                    <input type="file" accept=".pdf" @change="handleFileUpload($event, 'cv')"
+                                        class="w-full border rounded px-3 py-2 text-xs md:text-sm" />
+                                    <p v-if="errors.cv" class="text-red-500 text-xs md:text-sm mt-1">
                                         {{ errors.cv[0] }}
                                     </p>
                                 </div>
                                 <!-- Lettre de motivation -->
                                 <div class="mb-4">
-                                    <label class="block text-gray-700">Lettre de motivation (optionnel)</label>
-                                    <input type="file" @change="handleFileUpload($event, 'cover_letter')"
-                                        class="w-full border rounded px-3 py-2" />
-                                    <p v-if="errors.cover_letter" class="text-red-500 text-sm mt-1">
+                                    <label class="block text-gray-700 text-xs md:text-sm">Lettre de motivation
+                                        (optionnel)</label>
+                                    <input type="file" accept=".pdf" @change="handleFileUpload($event, 'cover_letter')"
+                                        class="w-full border rounded px-3 py-2 text-xs md:text-sm" />
+                                    <p v-if="errors.cover_letter" class="text-red-500 text-xs md:text-sm mt-1">
                                         {{ errors.cover_letter[0] }}
                                     </p>
                                 </div>
-                                <!-- Submit Button -->
+                                <!-- Bouton de soumission -->
                                 <div class="flex justify-end">
                                     <button type="submit"
-                                        class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                                        class="bg-blue-500 text-white px-4 py-2 font-light rounded-lg text-sm md:text-base hover:bg-blue-600">
                                         Postuler
                                     </button>
                                 </div>
                             </form>
                         </div>
                     </div>
+                    <!-- Message pour les non-développeurs ou non connectés -->
+                    <p v-if="!(authStore.user && authStore.user.role_id === 1)"
+                        class="text-blue-500 text-xs md:text-base mt-4 text-center">
+                        Connectez-vous en tant que développeur pour postuler à cette offre.
+                    </p>
+                </div>
+                <!-- Modal Footer -->
+                <div class="flex justify-end px-6 py-4 border-t">
+                    <button @click="closeModal"
+                        class="bg-blue-500 text-white px-4 py-2 font-light rounded-lg text-sm md:text-base hover:bg-blue-600">
+                        Fermer
+                    </button>
                 </div>
             </div>
             <!-- Afficher un message de chargement ou d'erreur -->
             <div v-else class="p-6">
                 <p v-if="isLoading">Chargement...</p>
-                <p v-else-if="errorMessage" class="text-red-500">
-                    {{ errorMessage }}
-                </p>
+                <p v-else-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
             </div>
         </div>
     </div>
