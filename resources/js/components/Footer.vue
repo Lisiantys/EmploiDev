e648276a856d49d67ca929e41321e34a00fb36d7
Lisiantys <maxime.pouzargue7@gmail.com>
2024-10-14 00:45:56 +0200

    V3.2.7 - Footer, mentions et politiques

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -2,9 +2,9 @@
     <footer class="w-full bg-blue-500 p-4 md:pl-16 lg:pl-10 md:py-8 pb-24 md:mb-0">
       <div
         class="grid grid-flow-row gap-4 space-y-5 md:space-y-0 items-center justify-items-center text-center md:grid-flow-col md:text-left">
-        <div class="text-white max-w-md space-y-4 md:space-y-2">
+        <div class="text-white max-w-md space-y-4 md:space-y-2 pl-8 lg:pl-0">
           <h3 class="font-extrabold text-xl tracking-wider">EmploiDev</h3>
-          <p class="text-sm sm:text-base leading-7">
+          <p class="text-sm leading-7">
             Développez votre carrière avec EmploiDev, la plateforme de recrutement dédiée aux développeurs
             informatiques. Trouvez des opportunités d'alternance et des offres d'emploi en développement
             logiciel. Rejoignez des entreprises technologiques de pointe et explorez des carrières
@@ -16,25 +16,25 @@
           <li>
             <div class="link-list">
               <img :src="imageUrl" class="svg-icon" alt="#" />
-              <a href="#" class="hover:underline">Accueil </a>
+              <a href="#" class="hover:underline text-sm lg:text-base">Accueil </a>
             </div>
           </li>
           <li>
             <div class="link-list">
               <img :src="imageUrl" class="svg-icon" alt="#" />
-              <a href="#" class="hover:underline">Contactez-nous</a>
+              <a href="#" class="hover:underline text-sm lg:text-base">Contactez-nous</a>
             </div>
           </li>
           <router-link :to="{ name: 'mentionsLegales' }">
             <div class="link-list">
               <img :src="imageUrl" class="svg-icon" alt="#" />
-              <a href="#" class="hover:underline">Mentions Légales</a>
+              <a href="#" class="hover:underline text-sm lg:text-base">Mentions Légales</a>
             </div>
           </router-link>
           <router-link :to="{ name: 'politiqueConfidentialite' }">
             <div class="link-list">
               <img :src="imageUrl" class="svg-icon" alt="#" />
-              <a href="#" class="hover:underline">Politique de Confidentialité</a>
+              <a href="#" class="hover:underline text-sm lg:text-base">Politique de Confidentialité</a>
             </div>
           </router-link>
         </ul>
--- resources/js/components/MentionsLegales.vue
+++ resources/js/components/MentionsLegales.vue
@@ -1,44 +1,62 @@
 <template title="Mentions Légales">
-    <div class="p-6 sm:p-10 md:pl-32">
-        <div class="space-y-4">
-            <h2 class="font-semibold text-lg md:text-2xl">Propriétaire du site : </h2>
 
-            <p>EI Maxime POUZARGUE</p>
 
-            <p>Adresse : 237 Bis Avenue François Mitterand, 85340 Les Sables d'Olonne.</p>
-
-            <p>Contact : contact@winterbike24.fr OU 06 27 34 07 55</p>
-        </div>
-
-        <div class="space-y-4 mt-5">
-            <h2 class="font-semibold text-lg md:text-2xl">Propriétés intellectuelles</h2>
-            <p class="leading-9">
-                Maxime POUZARGUE est propriétaire des droits de propriété intellectuelle et détient les droits d’usage
-                sur les éléments accessibles sur le site internet, notamment les textes, et les logos.
-                <br>
-                Pour l'image de profil utilisateur :
-                Flaticon : <a href="https://www.flaticon.com/"
-                    class="text-dark-green font-semibold">https://www.flaticon.com/</a>
-                <br>
-                Pour les icons :
-                Font Awesome : <a href="https://fontawesome.com/"
-                    class="text-dark-green font-semibold">https://fontawesome.com/</a>
-                <br>
-                Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments
-                du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable
-                de Maxime POUZARGUE.
-                <br>
-                Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera
-                considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles
-                L.335-2 et suivants du Code de Propriété Intellectuelle.
-            </p>
-        </div>
-
-        <div class="space-y-4 mt-5">
-            <h2 class="font-semibold text-lg md:text-2xl">Hébergeur :</h2>
-            <p>o2switch – <a href="https://www.o2switch.fr/"
-                    class="text-dark-green font-semibold">https://www.o2switch.fr/</a> Chem. des Pardiaux, 63000
-                Clermont-Ferrand – Téléphone : 0444446040</p>
-        </div>
+  <div class="hidden md:block mt-20 mb-6">
+        <PageTitle title="// Mentions Légales" />
     </div>
+
+    <div class="space-y-4 mt-20 md:mt-0">
+        <h2 class="font-semibold text-lg md:text-2xl">Propriétaire du site : </h2>
+
+        <p>EI Maxime POUZARGUE</p>
+
+        <p>Adresse : 237 Bis Avenue François Mitterand, 85340 Les Sables d'Olonne.</p>
+
+        <p>Contact : contact@winterbike24.fr</p>
+    </div>
+
+    <div class="space-y-4 mt-5">
+        <h2 class="font-semibold text-lg md:text-2xl">Propriétés intellectuelles</h2>
+        <p class="leading-9">
+            Maxime POUZARGUE est propriétaire des droits de propriété intellectuelle et détient les droits d’usage
+            sur les éléments accessibles sur le site internet, notamment les textes, et les logos.
+            <br>
+            Pour les icons :
+            Font Awesome : <a href="https://fontawesome.com/"
+                class="text-dark-green font-semibold">https://fontawesome.com/</a>
+            <br>
+            Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments
+            du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable
+            de Maxime POUZARGUE.
+            <br>
+            Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera
+            considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles
+            L.335-2 et suivants du Code de Propriété Intellectuelle.
+        </p>
+    </div>
+
+    <div class="space-y-4 mt-5">
+        <h2 class="font-semibold text-lg md:text-2xl">Hébergeur :</h2>
+        <p>o2switch – <a href="https://www.o2switch.fr/"
+                class="text-dark-green font-semibold">https://www.o2switch.fr/</a> Chem. des Pardiaux, 63000
+            Clermont-Ferrand – Téléphone : 0444446040</p>
+    </div>
+
 </template>
+
+
+<script setup>
+import PageTitle from './PageTitle.vue'; 
+</script>
+
+<style scoped>
+p {
+    font-size: 14px;
+}
+
+@media screen and (max-width: 768px) {
+    p {
+        font-size: 12px;
+    }
+}
+</style>
