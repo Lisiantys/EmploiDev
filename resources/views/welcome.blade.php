6f32843906730c107917952fb2e52d25b4535fe6
Lisiantys <maxime.pouzargue7@gmail.com>
2024-10-14 11:37:39 +0200

    V3.2.9 - Ajout favicon (peut etre a revoir)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>EmploiDev</title>
-        <meta name="description" content="Rassemblements moto en France, balade, exposition, hivernale, roadtrip, chaque événement se trouve ici."> 
+        <meta name="description" content="EmploiDev est une plateforme dédiée à la mise en relation entre développeurs en recherche d'emploi ou d'alternance et entreprises du secteur technologique. Simplifiez le recrutement en trouvant des talents du numérique correspondant à vos critères, et accédez à de nouvelles opportunités de carrière dans le développement informatique.">
+
+        <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-48x48.png') }}" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon/favicon.svg') }}" />
+        <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}" />
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}" />
+        <meta name="apple-mobile-web-app-title" content="EmploiDev" />
+        <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}" />
 
         <!-- Styles -->
         @vite('resources/css/app.css')
